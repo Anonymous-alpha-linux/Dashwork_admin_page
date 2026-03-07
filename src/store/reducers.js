e35a1ab7078ea3442c02c177ddde90478787ddbe
Anonymous-alpha-linux <tinhnt1006@gmail.com>
2026-03-08 06:10:47 +0700

    Add Layout with Vertical sight

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,17 +1,25 @@
 import { combineReducers } from "redux";
 
+// Layout
+import Layout from "./layouts/reducer";
+
 // Authentication
 import Login from "./auth/login/reducer";
 import Account from "./auth/register/reducer";
 import ForgetPassword from "./auth/forgetpwd/reducer";
 import Profile from "./auth/profile/reducer";
 
+// Dashboard Ecommerce
+import DashboardEcommerce from "./dashboardEcommerce/reducer";
+
 const rootReducer = combineReducers({
   // public
   Login,
   Account,
   ForgetPassword,
-  Profile
+  Profile,
+  DashboardEcommerce,
+  Layout
 });
 
 export default rootReducer;
